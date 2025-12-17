ef137d55d1036bf1b8a9a8e28aefacc05c4c99c3
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2025-12-17 15:42:46 +0530

    Update user schema with isAdmin and connections
    Added isAdmin and connections fields directly in git.

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ let userSchema = mongoose.Schema({
     repeatpassword: String,
     isAdmin: false, // not in vs, added directly in git
     connections: [],  // not in vs, added directly in git
-    chatRooms: []
+    chatRooms: []  // not in vs, added directly in git
 });
 
 module.exports = mongoose.model('user', userSchema);
